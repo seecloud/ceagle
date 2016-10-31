c6ecea09b9f61ded41814ad18d96f27afea79725
Alexander Maretskiy <amaretskiy@mirantis.com>
2016-10-31 19:00:34 +0200

    Refactoring in ceagle/api/v1

--- ceagle/api/v1/cloud_status.py
+++ ceagle/api/v1/cloud_status.py
@@ -13,10 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import json
-
 import flask
-import requests
 
 from ceagle.api_fake_data import cloud_status_data
 
@@ -37,121 +34,17 @@ def get_blueprints():
 
 
 @overview.route("/")
-def overview_index():
-    return flask.jsonify({"result": {"cloud_status": "index"}})
-
-
-@overview.route("/v1")
-@overview.route("/v1/")
-def overview_data_v1():
-    return cloud_status_data.overview_data()
+def get_status():
+    return flask.jsonify(cloud_status_data.overview_data())
 
 
 @health.route("/", defaults={"region": "all"})
 @health.route("/<region>")
-def health_index(region):
-    return flask.jsonify({"result": {"cloud_status": "health"}})
-
-
-@health.route("/v1", defaults={"region": "all"})
-@health.route("/v1/<region>")
-def health_data_v1(region):
-    return cloud_status_data.health_projects()
-
-    period = flask.request.args.get("period", "day")
-
-    if period == "day":
-        period = "now-1d/m"
-        interval = "10m"
-    elif period == "week":
-        period = "now-7d/m"
-        interval = "1h"
-    else:
-        period = "now-30d/m"
-        interval = "4h"
-
-    query = {
-        "query": {
-            "filtered": {
-                "filter": {
-                    "range": {
-                        "timestamp": {
-                            "gte": period
-                        }
-                    }
-                }
-            }
-        },
-        "aggs": {
-            "projects": {
-                "terms": {"field": "service"},
-
-                "aggs": {
-                    "avg_fci": {
-                        "avg": {
-                            "field": "fci"
-                        }
-                    },
-                    "data": {
-                        "date_histogram": {
-                            "field": "timestamp",
-                            "interval": interval,
-                            "format": "yyyy-MM-dd'T'hh:mm",
-                            "min_doc_count": 0
-                        },
-                        "aggs": {
-                            "fci": {
-                                "avg": {"field": "fci"}
-                            },
-                            "response_size": {
-                                "avg": {"field": "response_time.avg"}
-                            },
-                            "response_time": {
-                                "avg": {"field": "response_size.avg"}
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    # TODO(boris-42): support regions
-    request = flask.current_app.config[
-        "cloud_status"]["regions"][0]["health"]["elastic"]
-    r = requests.get("%s/_search?search_type=count" % request,
-                     data=json.dumps(query))
-
-    result = {
-        "project_names": [],
-        "projects": {}
-    }
-
-    def convert_(data, field):
-        result = []
-        for d in data["buckets"]:
-            result.append([d["key_as_string"], d[field]["value"]])
-        return result
-
-    for project in r.json()["aggregations"]["projects"]["buckets"]:
-        result["project_names"].append(project["key"])
-        result["projects"][project["key"]] = {
-            "fci": project["avg_fci"]["value"],
-            "fci_score_data": convert_(project["data"], "fci"),
-            "response_time_data": convert_(project["data"], "response_time"),
-            "response_size_data": convert_(project["data"], "response_size")
-        }
-
-    return flask.jsonify(**result)
+def get_health(region):
+    return flask.jsonify(cloud_status_data.health_projects())
 
 
 @availability.route("/", defaults={"region": "all"})
 @availability.route("/<region>")
-def availability_index(region):
-    return flask.jsonify({"result": {"cloud_status": "availability"}})
-
-
-@availability.route("/v1", defaults={"region": "all"})
-@availability.route("/v1/<region>")
-def availability_data_v1(region):
-    return cloud_status_data.availability_data()
+def get_availability(region):
+    return flask.jsonify(cloud_status_data.availability_data())
