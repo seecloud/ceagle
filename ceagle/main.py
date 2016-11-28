dd49853bffce763614b4819af27a8167606f7502
Ilya Kharin <akscram@gmail.com>
2016-11-28 17:00:41 +0300

    Flask does not accept HOST and PORT from config
    The development server does not suppose to get parameters HOST and PORT
    from the configuration, they have to be set they have to be set
    explicitly as arguments for the run function. The full list of supported
    values is listed below:
    
    http://flask.pocoo.org/docs/0.11/config/#builtin-configuration-values

--- ceagle/main.py
+++ ceagle/main.py
@@ -26,8 +26,12 @@ from ceagle.api.v1 import status
 from ceagle import config
 
 
+CONF = config.get_config()
+APP_CONF = CONF["flask"]
+
+
 app = flask.Flask(__name__, static_folder=None)
-app.config.update(config.get_config()["flask"])
+app.config.update(APP_CONF)
 
 
 @app.route("/api/")
@@ -50,7 +54,7 @@ app = routing.add_routing_map(app, html_uri=None, json_uri="/")
 
 
 def main():
-    app.run()
+    app.run(host=APP_CONF["HOST"], port=APP_CONF["PORT"])
 
 
 if __name__ == "__main__":
